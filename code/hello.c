502bd8b03f1832f8b13f86bbd268c678535a935d
mcxiaoke <mcxiaoke@gmail.com>
2016-01-03 14:46:45 +0000

    add simple Makefile

--- code/hello.c
+++ code/hello.c
@@ -2,12 +2,12 @@
 * @Author: mcxiaoke
 * @Date:   2015-12-28 22:45:13
 * @Last Modified by:   mcxiaoke
-* @Last Modified time: 2015-12-28 23:15:59
+* @Last Modified time: 2016-01-03 22:05:36
 */
 
-#include <apue.h>
+#include <stdio.h>
 
-int main() {
+int main(int argc, char* argv[]) {
     printf("hello world, process id is %d, user id is %d\n", getpid(), getuid());
     return 0;
 }
